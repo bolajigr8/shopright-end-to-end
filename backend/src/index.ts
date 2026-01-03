e3650c32a6326180cabb75f57cd9b547a4fa6b00
Bolaji <badebayo11@gmail.com>
2026-01-04 00:18:05 +0100

    cart routes updates and cors setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,6 +11,8 @@ import userRoutes from './routes/user.route.js'
 import orderRoutes from './routes/order.route.js'
 import reviewsRoutes from './routes/review.route.js'
 import productsRoutes from './routes/product.route.js'
+import cartRoutes from './routes/cart.route.js'
+import cors from 'cors'
 
 const app = express()
 
@@ -24,11 +26,14 @@ app.use(clerkMiddleware())
 // Inngest webhook endpoint
 app.use('/api/inngest', serve({ client: inngest, functions }))
 
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })) // credentials: true allows the browser to send the cookies to the server with the request
+
 app.use('/api/admin', adminRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/reviews', reviewsRoutes)
 app.use('/api/products', productsRoutes)
+app.use('/api/cart', cartRoutes)
 
 app.get('/api/health', (req, res) => {
   res.send('Hello, World!')
