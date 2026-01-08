b9a40b74022202d55671016afe8f1ecc8dfc69f4
Bolaji <badebayo11@gmail.com>
2026-01-08 02:22:58 +0100

    copied all the mobile code from the tutorial source code for mobile and pasted bc of peer dependency errors

--- mobile/app/(auth)/_layout.tsx
+++ mobile/app/(auth)/_layout.tsx
@@ -1,12 +1,14 @@
-import { Redirect, Stack } from 'expo-router'
-import { useAuth } from '@clerk/clerk-expo'
+import { Redirect, Stack } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 
 export default function AuthRoutesLayout() {
-  const { isSignedIn } = useAuth()
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) return null; // for a better ux
 
   if (isSignedIn) {
-    return <Redirect href={'/'} />
+    return <Redirect href={"/(tabs)"} />;
   }
 
-  return <Stack screenOptions={{ headerShown: false }} />
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
